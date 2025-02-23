bc43c242ff86d9725b31fddc0cd167e6f3b1b5d0
tocha_mateusz <mateusz@primea.se>
2025-02-23 20:19:30 +0100

    feat(roll): change way of rolling camera by mouse

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,142 @@
-//! Demonstrates all common configuration options,
-//! and how to modify them at runtime
+//! Shows how to orbit camera around a static scene using pitch, yaw, and roll.
 //!
-//! Controls:
-//!   Orbit: Middle click
-//!   Pan: Shift + Middle click
-//!   Zoom: Mousewheel
+//! See also: `first_person_view_model` example, which does something similar but as a first-person
+//! camera view.
 
-use bevy::prelude::*;
-use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin, TouchControls};
-use std::f32::consts::TAU;
+use std::{f32::consts::FRAC_PI_2, ops::Range};
+
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
+
+#[derive(Debug, Resource)]
+struct CameraSettings {
+    pub orbit_distance: f32,
+    pub pitch_speed: f32,
+    // Clamp pitch to this range
+    pub pitch_range: Range<f32>,
+    pub roll_speed: f32,
+    pub yaw_speed: f32,
+}
+
+impl Default for CameraSettings {
+    fn default() -> Self {
+        // Limiting pitch stops some unexpected rotation past 90° up or down.
+        let pitch_limit = FRAC_PI_2 - 0.01;
+        Self {
+            // These values are completely arbitrary, chosen because they seem to produce
+            // "sensible" results for this example. Adjust as required.
+            orbit_distance: 20.0,
+            pitch_speed: 0.003,
+            pitch_range: -pitch_limit..pitch_limit,
+            roll_speed: 1.0,
+            yaw_speed: 0.004,
+        }
+    }
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(PanOrbitCameraPlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, toggle_camera_controls_system)
+        .init_resource::<CameraSettings>()
+        .add_systems(Startup, (setup, instructions))
+        .add_systems(Update, orbit)
         .run();
 }
 
+/// Set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // Ground
     commands.spawn((
+        Name::new("Camera"),
+        Camera3d::default(),
+        Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    commands.spawn((
+        Name::new("Plane"),
         Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
-        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
-    ));
-    // Cube
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-    ));
-    // Light
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.3, 0.5, 0.3),
+            // Turning off culling keeps the plane visible when viewed from beneath.
+            cull_mode: None,
             ..default()
-        },
-        Transform::from_xyz(4.0, 8.0, 4.0),
+        })),
     ));
-    // Camera
+
     commands.spawn((
-        // Note we're setting the initial position below with yaw, pitch, and radius, hence
-        // we don't set transform on the camera.
-        PanOrbitCamera {
-            // Set focal point (what the camera should look at)
-            focus: Vec3::new(0.0, 1.0, 0.0),
-            // Set the starting position, relative to focus (overrides camera's transform).
-            yaw: Some(TAU / 8.0),
-            pitch: Some(TAU / 8.0),
-            radius: Some(5.0),
-            // Set limits on rotation and zoom
-            yaw_upper_limit: Some(TAU / 4.0),
-            yaw_lower_limit: Some(-TAU / 4.0),
-            pitch_upper_limit: Some(TAU / 3.0),
-            pitch_lower_limit: Some(-TAU / 3.0),
-            zoom_upper_limit: Some(5.0),
-            zoom_lower_limit: 1.0,
-            // Adjust sensitivity of controls
-            orbit_sensitivity: 1.5,
-            pan_sensitivity: 0.5,
-            zoom_sensitivity: 0.5,
-            // Allow the camera to go upside down
-            allow_upside_down: true,
-            // Change the controls (these match Blender)
-            button_orbit: MouseButton::Middle,
-            button_pan: MouseButton::Middle,
-            modifier_pan: Some(KeyCode::ShiftLeft),
-            // Reverse the zoom direction
-            reversed_zoom: true,
-            // Use alternate touch controls
-            touch_controls: TouchControls::TwoFingerOrbit,
+        Name::new("Cube"),
+        Mesh3d(meshes.add(Cuboid::default())),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(1.5, 0.51, 1.5),
+    ));
+
+    commands.spawn((
+        Name::new("Light"),
+        PointLight::default(),
+        Transform::from_xyz(3.0, 8.0, 5.0),
+    ));
+}
+
+fn instructions(mut commands: Commands) {
+    commands.spawn((
+        Name::new("Instructions"),
+        Text::new(
+            "Mouse up or down: pitch\n\
+            Mouse left or right: yaw\n\
+            Mouse buttons: roll",
+        ),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.),
+            left: Val::Px(12.),
             ..default()
         },
     ));
 }
 
-// This is how you can change config at runtime.
-// Press 'T' to toggle the camera controls.
-fn toggle_camera_controls_system(
-    key_input: Res<ButtonInput<KeyCode>>,
-    mut pan_orbit_query: Query<&mut PanOrbitCamera>,
+fn orbit(
+    mut camera: Single<&mut Transform, With<Camera>>,
+    camera_settings: Res<CameraSettings>,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+    time: Res<Time>,
 ) {
-    if key_input.just_pressed(KeyCode::KeyT) {
-        for mut pan_orbit in pan_orbit_query.iter_mut() {
-            pan_orbit.enabled = !pan_orbit.enabled;
-        }
+    let mut delta = Vec2 { x: 0.0, y: 0.0 };
+    //
+    let mut delta_roll = 0.0;
+
+    if mouse_buttons.pressed(MouseButton::Left) {
+        delta = -mouse_motion.delta;
     }
+    if mouse_buttons.pressed(MouseButton::Right) {
+        delta_roll += 1.0;
+    }
+
+    // Mouse motion is one of the few inputs that should not be multiplied by delta time,
+    // as we are already receiving the full movement since the last frame was rendered. Multiplying
+    // by delta time here would make the movement slower that it should be.
+    let delta_pitch = delta.y * camera_settings.pitch_speed;
+    let delta_yaw = delta.x * camera_settings.yaw_speed;
+
+    // Conversely, we DO need to factor in delta time for mouse button inputs.
+    delta_roll *= camera_settings.roll_speed * time.delta_secs();
+
+    // Obtain the existing pitch, yaw, and roll values from the transform.
+    let (yaw, pitch, roll) = camera.rotation.to_euler(EulerRot::YXZ);
+
+    // Establish the new yaw and pitch, preventing the pitch value from exceeding our limits.
+    let pitch = (pitch + delta_pitch).clamp(
+        camera_settings.pitch_range.start,
+        camera_settings.pitch_range.end,
+    );
+    let roll = roll + delta_roll;
+    let yaw = yaw + delta_yaw;
+    camera.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+
+    // Adjust the translation to maintain the correct orientation toward the orbit target.
+    // In our example it's a static target, but this could easily be customized.
+    let target = Vec3::ZERO;
+    camera.translation = target - camera.forward() * camera_settings.orbit_distance;
 }
